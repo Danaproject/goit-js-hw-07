f9e1954d1be59b8c6e7e01da48de77b09694ba4f
Dana Project <decoratio@gmail.com>
2021-01-21 20:09:02 +0100

    task 8 done

---
+++ js/task-08.js
@@ -0,0 +1,28 @@
+const inputRef = document.querySelector('#controls > input[type=number]');
+const renderBtnRef = document.querySelector('button[data-action="render"]');
+const destroyBtnRef = document.querySelector('button[data-action="destroy"]');
+const boxesRef = document.querySelector('#boxes');
+
+let initialSize = 30;
+    
+function createBoxes(amount) {
+    for (let i = 0; i < inputRef.value; i += 1) {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        const newBox = document.createElement('div');
+        newBox.style.backgroundColor = "#" + randomColor;;
+        newBox.style.width = `${initialSize}px`;
+        newBox.style.height = `${initialSize}px`;
+        boxesRef.appendChild(newBox);
+
+        initialSize += 10;
+    }
+    return boxesRef;
+}
+
+function destroyBoxes() {
+    boxesRef.innerHTML = '';
+}
+renderBtnRef.addEventListener('click', createBoxes);
+destroyBtnRef.addEventListener('click', destroyBoxes);
+
+
